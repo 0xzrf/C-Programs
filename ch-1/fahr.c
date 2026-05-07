5a0e347e1cb1e8005e9dd33bd0e731a959d0ef75
Zeref <zeref18.drgnl@gmail.com>
2026-05-07 11:59:11 +0530

    added for loop fahr_to_cel

--- ch-1/fahr.c
+++ ch-1/fahr.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 #define LOWER 0
-#define UPPER 200
+#define UPPER 300
 #define STEP 20
+
 void print_fahr_to_cel();
 void print_cel_to_fahr();
+void print_fahr_to_cel_for();
+
 int main() {
-  print_fahr_to_cel();
-  print_cel_to_fahr();
+  // print_fahr_to_cel();
+  // print_cel_to_fahr();
+  print_fahr_to_cel_for();
 }
 
 void print_fahr_to_cel() {
@@ -33,5 +37,14 @@ void print_cel_to_fahr() {
     fahr = (cel * (9.0 / 5.0)) + 32.0;
     printf("%3.0f\t%6.1f\n", cel, fahr);
     cel += STEP;
+  }
+}
+
+void print_fahr_to_cel_for() {
+  int fahr;
+
+  printf("Fahr\t Celsius\n");
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - 20) {
+    printf("%3d\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
   }
 }
