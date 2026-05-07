82cb5059abc1723af27b604a5880b8ad95537979
Zeref <zeref18.drgnl@gmail.com>
2026-05-07 12:34:30 +0530

    added rm_mult_blanks fn

--- ch-1/counter.c
+++ ch-1/counter.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-		
-int main() {
+
+void print_blanks_nl_tabs();
+void rm_mult_blanks();
+
+int main() { rm_mult_blanks(); }
+
+void print_blanks_nl_tabs() {
   long blanks, tabs, nl;
   int c;
   blanks = tabs = nl = 0;
@@ -15,4 +20,21 @@ int main() {
   }
 
   printf("Blanks: %2ld\tTabs: %2ld\tNewlines: %2ld\n", blanks, tabs, nl);
+}
+
+void rm_mult_blanks() {
+  char at_least_one_blank = 0; // Initially, this is set to false
+  int c;
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      if (!at_least_one_blank) { // we already encountered a blank
+        putchar(c);
+        at_least_one_blank = 1;
+      }
+    } else {
+      putchar(c);
+      at_least_one_blank = 0;
+    }
+  }
+  printf("\n");
 }
