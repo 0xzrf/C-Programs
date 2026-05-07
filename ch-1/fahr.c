d227f335eb60e1540694604dc138b11871f7c9f1
Zeref <zeref18.drgnl@gmail.com>
2026-05-07 11:50:12 +0530

    added print_cel_from_feht

--- ch-1/fahr.c
+++ ch-1/fahr.c
@@ -3,17 +3,35 @@
 #define LOWER 0
 #define UPPER 200
 #define STEP 20
-
+void print_fahr_to_cel();
+void print_cel_to_fahr();
 int main() {
-  int fahr, cel;
+  print_fahr_to_cel();
+  print_cel_to_fahr();
+}
+
+void print_fahr_to_cel() {
+  float fahr, cel;
 
   fahr = LOWER;
 
   printf("Celsius\t   Fahr\n");
-  ;
   while (fahr <= UPPER) {
-    cel = 5 * (fahr - 32) / 9;
-    printf("%3d\t%6d\n", cel, fahr);
+    cel = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.1f\t%6.0f\n", cel, fahr);
     fahr += STEP;
+  }
+}
+
+void print_cel_to_fahr() {
+  float fahr, cel;
+
+  cel = LOWER;
+
+  printf("Celsius\t   Fahr\n");
+  while (cel <= UPPER) {
+    fahr = (cel * (9.0 / 5.0)) + 32.0;
+    printf("%3.0f\t%6.1f\n", cel, fahr);
+    cel += STEP;
   }
 }
