600ddb34910b3cfc9adc3152d7f9d4d5ebda1f2b
Zeref <zeref18.drgnl@gmail.com>
2026-05-07 13:42:28 +0530

    added a histogram

--- ch-1/arrays.c
+++ ch-1/arrays.c
@@ -3,8 +3,9 @@
 #define DIGITS 10
 
 void digit_counter();
+void print_histogram_horizontal();
 
-int main() { digit_counter(); }
+int main() { print_histogram_horizontal(); }
 
 void digit_counter() {
   int ndigits[DIGITS], c;
@@ -22,5 +23,29 @@ void digit_counter() {
   printf("Digits count\n");
   for (char i = 0; i < DIGITS; i++) {
     printf("%d: %d\n", i, ndigits[i]);
+  }
+}
+
+void print_histogram_horizontal() {
+  int ndigits[DIGITS], c;
+
+  for (char i = 0; i < DIGITS; i++) {
+    ndigits[i] = 0;
+  }
+
+  while ((c = getchar()) != EOF) {
+    if (c >= '0' || c <= '9') {
+      ++ndigits[c - '0'];
+    }
+  }
+
+  printf("Digits count\n");
+  for (char i = 0; i < DIGITS; i++) {
+    printf("%3d: ", i);
+
+    for (char j = 0; j < ndigits[i]; j++) {
+      printf("=");
+    }
+    printf("\n");
   }
 }
