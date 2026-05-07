8d8bc9e1b135ed9cc7a16dc1bd2ab0674909d0c8
Zeref <zeref18.drgnl@gmail.com>
2026-05-07 11:27:19 +0530

    added fahr-to-cal

--- ch-1/fahr.c
+++ ch-1/fahr.c
@@ -9,11 +9,11 @@ int main() {
 
   fahr = LOWER;
 
-  printf("Celsius\tFahr");
+  printf("Celsius\t   Fahr\n");
   ;
   while (fahr <= UPPER) {
     cel = 5 * (fahr - 32) / 9;
-    printf("%d\t%d", cel, fahr);
+    printf("%3d\t%6d\n", cel, fahr);
     fahr += STEP;
   }
 }
